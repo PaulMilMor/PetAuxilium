cd9d7d68a96ae976e9678a3a85a465d11480cb68
FelixGtz <felix1699@gmail.com>
2021-02-15 12:49:28 -0700

    Tema

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Container(child:Icon(Icons.access_alarm_sharp))
+    );
+  }
+}
