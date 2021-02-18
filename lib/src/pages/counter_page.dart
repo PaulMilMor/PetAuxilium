35f4a9fa2514694841400045089bc57122d66aff
Daniel Alfaro <a216203326@unison.mx>
2021-02-17 22:40:19 -0700

    feed- 17/02/2021

---
+++ lib/src/pages/counter_page.dart
@@ -0,0 +1,208 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListViewPage(),
+    );
+  }
+}
+
+// Main Stateful Widget Start
+class ListViewPage extends StatefulWidget {
+  @override
+  _ListViewPageState createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  // Title List Here
+  var titleList = [
+    "Roberto",
+    "Felipe",
+    "Francisco",
+    "Natalia",
+    "Carlitos",
+    "Federico",
+    "Mariana"
+  ];
+  var subtitleList = [
+    "SE BUSCA",
+    "VETERINARIA",
+    "CUIDADOR",
+    "SE BUSCA",
+    "VETERINARIA",
+    "ADOPCION",
+    "CUIDADORA"
+  ];
+
+  // Description List Here
+  var descList = [
+    "Lorem ipsum dolor sit amet",
+    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium",
+    "architecto beatae vitae dicta sunt explicabo.",
+    "magni dolores eos qui ratione voluptatem sequi nesciunt.",
+    "quo voluptas nulla pariatur?",
+    "dolorem ipsum quia dolor sit amet,",
+    "Duis aute irure dolor in reprehenderit"
+  ];
+
+  // Image Name List Here
+  var imgList = [
+    "assets/images/img1.jpg",
+    "assets/images/img2.jpg",
+    "assets/images/img3.jpg",
+    "assets/images/img4.jpg",
+    "assets/images/img5.jpg",
+    "assets/images/img6.jpg",
+    "assets/images/img7.jpg"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    // MediaQuery to get Device Width
+    double width = MediaQuery.of(context).size.width * 0.6;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PetAuxulium'),
+        centerTitle: true,
+      ),
+      // Main List View With Builder
+      body: ListView.builder(
+        itemCount: imgList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              // This Will Call When User Click On ListView Item
+              showDialogFunc(context, imgList[index], titleList[index],
+                  subtitleList[index], descList[index]);
+            },
+            // Card Which Holds Layout Of ListView Item
+            child: Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    child: Image.asset(imgList[index]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          titleList[index],
+                          style: TextStyle(
+                            fontSize: 21,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          subtitleList[index],
+                          style: TextStyle(
+                            fontSize: 11,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 21,
+                        ),
+                        Container(
+                          width: width,
+                          child: Text(
+                            descList[index],
+                            maxLines: 3,
+                            style: TextStyle(
+                                fontSize: 15, color: Colors.grey[500]),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 21,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+// This is a block of Model Dialog
+showDialogFunc(context, img, title, subtitle, desc) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return Center(
+        child: Material(
+          type: MaterialType.transparency,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+            ),
+            padding: EdgeInsets.all(50),
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Image.asset(
+                    img,
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  subtitle,
+                  style: TextStyle(
+                    fontSize: 11,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  // width: 200,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      desc,
+                      maxLines: 3,
+                      style: TextStyle(fontSize: 15, color: Colors.grey[500]),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+  );
+}
