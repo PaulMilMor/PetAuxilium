f19f0ca999967e8daa19e2945faa1723165e8962
Sebas <sebastianrnoriega.hotmail.com>
2021-02-19 00:46:18 -0700

    el chilo

---
+++ lib/models/publication_model.dart
@@ -0,0 +1,10 @@
+class AddAdoption{
+String id;
+String category;
+String name;
+String description;
+String ubication;
+String imgRef;
+AddAdoption({this.id,this.category,this.name,this.description, this.ubication, this.imgRef});
+
+}
