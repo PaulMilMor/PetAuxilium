0da23d3ae4b0b51bd4705aa5e88de437a8fd2a51
Daniel Alfaro <a216203326@unison.mx>
2021-03-10 18:06:33 -0700

    not_yet

---
+++ lib/pages/opinion_page.dart
@@ -0,0 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:pet_auxilium/models/publication_model.dart';
