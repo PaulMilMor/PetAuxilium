16796ce2d8d505892fd25fc6ca261c19bc871036
FelixGtz <felix1699@gmail.com>
2021-05-08 14:52:58 -0700

    fix error patreon menu

--- lib/pages/paid_options_page.dart
+++ lib/pages/paid_options_page.dart
@@ -415,7 +415,7 @@ class _PaidOptionsPageState extends State<PaidOptionsPage> {
             padding: EdgeInsets.fromLTRB(1, 1, 10, 20),
             child: GestureDetector(
                 onTap: () {
-                  Navigator.pushNamed(context, 'mypublicationsPage');
+                  Navigator.pop(context);
                 },
                 child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
                   Text('Continuar de forma gratuita'),
